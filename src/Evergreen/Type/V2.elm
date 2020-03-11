7e780f4deea6a9856147341e0d0013e75bb5abb3
Drew <dlazzeri1@gmail.com>
2020-03-11 04:40:23 +0100

    Adding lamdera file

---
+++ src/Evergreen/Type/V2.elm
@@ -0,0 +1,45 @@
+module Evergreen.Type.V2 exposing (..)
+
+import Browser exposing (UrlRequest)
+import Browser.Navigation exposing (Key)
+import Dict exposing (Dict)
+import Lamdera
+import Set exposing (Set)
+import Url exposing (Url)
+
+
+type alias FrontendModel =
+    { key : Key
+    , socks : Maybe Socks
+    }
+
+
+type alias Socks =
+    Dict.Dict String Int
+
+
+type alias BackendModel =
+    { socks : Socks
+    , clients : Set Lamdera.ClientId
+    }
+
+
+type FrontendMsg
+    = UrlClicked UrlRequest
+    | UrlChanged Url
+    | NoOpFrontendMsg
+    | Increment String
+
+
+type ToBackend
+    = SayHello
+    | SaveSocks Socks
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | BroadcastSocks Socks
