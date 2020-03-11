f7ad695c3df39a69d317c3fc87bb4a0983c21f74
Drew <dlazzeri1@gmail.com>
2020-03-11 11:28:30 +0100

    Preparing for v3

---
+++ src/Evergreen/Migrate/V3.elm
@@ -0,0 +1,42 @@
+module Evergreen.Migrate.V3 exposing (..)
+
+import Dict
+import Evergreen.Type.V2 as Old
+import Lamdera.Migrations exposing (..)
+import Evergreen.Type.V3 as New
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelUnchanged
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelMigrated
+        ( { message = "Test"
+          , clients = old.clients
+          , socks = Dict.fromList [ ( "drew", 0 ), ( "chris", 0 ) ]
+          }
+        , Cmd.none
+        )
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgUnchanged
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgUnchanged
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
